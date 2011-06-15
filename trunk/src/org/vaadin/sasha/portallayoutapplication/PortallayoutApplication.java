e0078ba705011d6fba605645ed52e04636a0eb8b
sasha.pchelintsev <sasha.pchelintsev@61862ca8-d77f-e084-32e4-20d6a289fcff>
2011-06-15 10:43:37 +0000

    Added the first draft of the component source to the repo: - Template for the widget. - Sample application. Update slightly the description of the competitors portal impls.

---
+++ trunk/src/org/vaadin/sasha/portallayoutapplication/PortallayoutApplication.java
@@ -0,0 +1,20 @@
+package org.vaadin.sasha.portallayoutapplication;
+
+import org.vaadin.sasha.portallayout.PortalLayout;
+
+import com.vaadin.Application;
+import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.Window;
+
+public class PortallayoutApplication extends Application {
+	@Override
+	public void init() {
+	  CssLayout l = new CssLayout();
+		Window mainWindow = new Window("Portallayout Application");
+		PortalLayout label = new PortalLayout();
+		mainWindow.addComponent(label);
+		setMainWindow(mainWindow);
+	}
+	
+	
+}
