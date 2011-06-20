fa6fb5befbf0e7c68e45792cc61314cdba859227
sasha.pchelintsev <sasha.pchelintsev@61862ca8-d77f-e084-32e4-20d6a289fcff>
2011-06-20 14:37:51 +0000

    Made some experimaental changes to the classes - practising with client-server communication.

--- src/org/vaadin/sasha/portallayout/PortalLayout.java
+++ src/org/vaadin/sasha/portallayout/PortalLayout.java
@@ -1,29 +1,53 @@
 package org.vaadin.sasha.portallayout;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
 import org.vaadin.sasha.portallayout.client.ui.VPortalLayout;
 
 import com.vaadin.terminal.PaintException;
 import com.vaadin.terminal.PaintTarget;
-import com.vaadin.ui.AbstractComponent;
+import com.vaadin.ui.AbstractLayout;
 import com.vaadin.ui.ClientWidget;
+import com.vaadin.ui.ClientWidget.LoadStyle;
+import com.vaadin.ui.Component;
 
 /**
  * Layout that presents its contents in a portal style. 
  * @author p4elkin
  */
 @SuppressWarnings("serial")
-@ClientWidget(VPortalLayout.class)
-public class PortalLayout extends AbstractComponent{
+@ClientWidget(value = VPortalLayout.class, loadStyle = LoadStyle.EAGER)
+public class PortalLayout extends AbstractLayout {
   
-  public PortalLayout() {
+  private List<Component> components = new LinkedList<Component>();
+  
+  private Map<Integer, List<Component>> columns = new HashMap<Integer, List<Component>>();
+  
+  private int columnCount = 0;
+  
+  public PortalLayout(int columncount) {
     super();
+    setWidth("95%");
+    setHeight("95%");
+    setColumnCount(columnCount);
   }
   
+  private void setColumnCount(int columnCount) {
+    this.columnCount = columnCount;
+    requestRepaint();
+  }
+
   @Override
   public void paintContent(PaintTarget target) throws PaintException {
-    super.paintContent(target);
+    target.addAttribute("cls", columnCount);
+    super.paintContent(target);  
+    for (final Component c : components)
+      c.paint(target);
   }
 
   /**
@@ -35,5 +59,35 @@ public class PortalLayout extends AbstractComponent{
     super.changeVariables(source, variables);
   }
 
+  @Override
+  public void replaceComponent(Component oldComponent, Component newComponent) {
+    // TODO Auto-generated method stub
+  }
 
+  @Override
+  public Iterator<Component> getComponentIterator() {
+    return Collections.unmodifiableCollection(components).iterator();
+  }
+
+  @Override
+  public void addComponent(Component c) {
+    addComponent(c, 0);
+  }
+
+    @Override
+    public void setWidth(float width, int unit) {
+      // TODO Auto-generated method stub
+      super.setWidth(width, unit);
+    }
+  
+  public void addComponent(Component c, int columnIdx)
+  {
+    //if (columns.size() < columnIdx)
+    //  throw new IllegalArgumentException();
+    super.addComponent(c);
+   // List<Component> columnComponents = columns.get(columnIdx);
+//    columnComponents.add(c);
+    components.add(c);
+    requestRepaint();
+  }
 }
--- src/org/vaadin/sasha/portallayoutapplication/PortallayoutApplication.java
+++ src/org/vaadin/sasha/portallayoutapplication/PortallayoutApplication.java
@@ -1,14 +1,17 @@
 package org.vaadin.sasha.portallayoutapplication;
 
+
 import org.vaadin.sasha.portallayout.PortalLayout;
 
 import com.vaadin.Application;
 import com.vaadin.event.dd.DropHandler;
 import com.vaadin.ui.AbsoluteLayout;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.CssLayout;
 import com.vaadin.ui.DragAndDropWrapper;
 import com.vaadin.ui.GridLayout;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
 
@@ -18,7 +21,6 @@ public class PortallayoutApplication extends Application {
 	public void init() {
 	  AbsoluteLayout llll;
 	  GridLayout grid;
-	  VerticalLayout ll = new VerticalLayout();
 	  CssLayout l = new CssLayout();
 	  l.setSizeFull();
 	  DropHandler handler;
@@ -26,10 +28,27 @@ public class PortallayoutApplication extends Application {
 	  
 		Window mainWindow = new Window("Portallayout Application");
 		mainWindow.getContent().setSizeFull();
-		PortalLayout label = new PortalLayout();
-		mainWindow.addComponent(new Button());
-		mainWindow.addComponent(label);
-		setMainWindow(mainWindow);
+		
+		VerticalLayout ll = (VerticalLayout) mainWindow.getContent();
+		
+		final PortalLayout portal = new PortalLayout(5);
+    Button b = new Button();
+    b.addListener(new Button.ClickListener() {
+      @Override
+      public void buttonClick(ClickEvent event) {
+        final TextField tf = new TextField();
+        tf.setWidth("200px");
+        portal.addComponent(tf);
+      }
+    });
+    
+    ll.addComponent(b);
+    ll.addComponent(portal);
+    ll.setExpandRatio(portal, 1.0f);
+    setMainWindow(mainWindow);
+    
+		//mainWindow.addComponent(ll);
+    //ll.addComponent(new Button());
 	}
 	
 	
