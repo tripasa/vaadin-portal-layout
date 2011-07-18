9d8449cc31a4039fbb80a2385eeab0ae9e30dd40
sasha.pchelintsev <sasha.pchelintsev@61862ca8-d77f-e084-32e4-20d6a289fcff>
2011-07-18 12:08:58 +0000

    Changed the sample sources. Previous ones slowed down functionality.

---
+++ trunk/WebContent/SampleSources/Client/PortallayoutApplication.java
@@ -0,0 +1,38 @@
+package org.vaadin.sasha.portalappdemo;
+
+import com.vaadin.Application;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Window;
+
+@SuppressWarnings("serial")
+public class PortallayoutApplication extends Application {
+
+  boolean debugMode = false;
+
+  private Window mainWindow;
+
+  @Override
+  public void init() {
+
+    setTheme("portallayouttheme");
+    mainWindow = new Window("Portallayout Application");
+    testInit();
+    setMainWindow(mainWindow);
+  }
+
+  public void testInit() {
+    HorizontalLayout windowLayout = new HorizontalLayout();
+    windowLayout.setSizeFull();
+
+    mainWindow.setContent(windowLayout);
+
+    PortalTabSheet tabs = new PortalTabSheet();
+    tabs.setSizeFull();
+    windowLayout.addComponent(tabs);
+    windowLayout.setExpandRatio(tabs, 1.0f);
+
+   // windowLayout.addComponent(sidePanel);
+
+  }
+
+}
